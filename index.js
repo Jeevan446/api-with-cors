26b533ddd7a35d31cd3cee52205be9d75c1bd963
jeevan446 <neupanejeevan446@gmail.com>
2024-12-13 11:42:03 +0545

    first commit

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ const express=require("express");
 const app=express();
 const PORT=process.env.PORT||3000;
 const cors=require("cors");
+app.use(cors({
+    origin:"*"
+}));
+// app.options('*', cors()); 
+
 const staticRouter=require("./routers/staticRouter");
 const productRouter=require("./routers/products");
 app.use("/",staticRouter);
 app.use("/products",productRouter);
-app.use(cors());
+
 app.listen(PORT,function(){
     console.log(`port is listening to ${PORT} port`);
 })
